77cbf122d4c7c78a0a8f23844e8c761cc1ffdb61
Ru-kko <verase2003.1@gmail.com>
2023-10-10 08:25:21 -0500

    feat(controller): signin and signup handlers

---
+++ services/auth/internal/handlers/sigIn.go
@@ -0,0 +1,88 @@
+package handlers
+
+import (
+	apierrors "Proyect-Y/api-errors"
+	"Proyect-Y/auth-service/internal/domain"
+	"Proyect-Y/auth-service/internal/middleware"
+	"Proyect-Y/auth-service/internal/security"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type tokenResponse struct {
+	Type       string `json:"toke_type"`
+	Token      string `json:"token"`
+	Expiration int64  `json:"expiration"`
+	UserID     string `json:"user_id"`
+	UserTag    string `json:"user_tag"`
+}
+
+func SignIn(c *gin.Context) {
+	var data domain.AuthCredentials
+	logger := logrus.New()
+
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, apierrors.BadRequest{
+			Name:    "BadRequest",
+			Message: "Bad information",
+		})
+		return
+	}
+
+	service, err := middleware.ReadDataInjection(c)
+	if err != nil {
+		logger.WithError(err).Error("Singin error: ")
+		c.JSON(http.StatusInternalServerError, apierrors.InternalServerError{
+			Name: "InjectionError",
+			Code: 500,
+		})
+	}
+
+	usr, err := service.GetUserByTag(data.UserTag)
+	if err != nil {
+		logger.WithError(err).Error("Singin error: ")
+		c.JSON(http.StatusInternalServerError, apierrors.InternalServerError{
+			Name: "DataBaseError",
+			Code: 500,
+		})
+		return
+	}
+
+	if usr == nil {
+		c.JSON(http.StatusNotFound, apierrors.UserNotFound{
+			Name: "UserNotFound",
+			User: data.UserTag,
+		})
+		return
+	}
+
+	err = security.VerifyPassword(data.Password, usr.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, apierrors.NotAuthorizedError{
+			Name:    "PasswordNotMatch",
+			Message: "Incorret password",
+		})
+		return
+	}
+
+	token, exp, err := security.BuildToken(*usr)
+	if err != nil {
+		logger.WithError(err).Error("Singin error: ")
+		c.JSON(http.StatusInternalServerError, apierrors.InternalServerError{
+			Code:    500,
+			Message: "Couldnot generate token",
+			Name:    "EncryptError",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, tokenResponse{
+		Type: "Bearer",
+		Token: token,
+		Expiration: exp,
+		UserID: usr.Id,
+		UserTag: usr.UserTag,
+	})
+}
---
+++ services/auth/internal/handlers/signUp.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	apierrors "Proyect-Y/api-errors"
+	"Proyect-Y/auth-service/internal/middleware"
+	"Proyect-Y/auth-service/internal/security"
+	"Proyect-Y/typo"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SignUp(c *gin.Context) {
+	var data typo.RegisterData
+	logger := logrus.New()
+
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, apierrors.BadRequest{
+			Name:    "BadRequest",
+			Message: err.Error(),
+		})
+		return
+	}
+
+	service, err := middleware.ReadDataInjection(c)
+	if err != nil {
+		logger.WithError(err).Error("singup error: ")
+		c.JSON(http.StatusInternalServerError, apierrors.InternalServerError{
+			Name: "InjectionError",
+			Code: 500,
+		})
+		return
+	}
+
+	user, err := service.UserRegister(data)
+	if err != nil {
+		if writeErr, ok := err.(mongo.WriteError); ok && writeErr.HasErrorCode(11000) {
+			c.JSON(http.StatusBadRequest, apierrors.BadRequest{
+				Name:    "BadRequest",
+				Message: "Repeated usertag or email",
+			})
+			return
+		}
+		logger.WithError(err).Error("singup error: ")
+		c.JSON(http.StatusInternalServerError, apierrors.InternalServerError{
+			Name: "InternalServerError",
+			Code: 500,
+		})
+		return
+	}
+
+	token, exp, err := security.BuildToken(*user)
+	if err != nil {
+		logger.WithError(err).Error("singup error: ")
+		c.JSON(http.StatusInternalServerError, apierrors.InternalServerError{
+			Name:    "SessionError",
+			Code:    500,
+			Message: "Couldn't create session token",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token, "id": user.Id, "type": "Bearer", "exp": exp, "user": user.UserTag})
+}
+
